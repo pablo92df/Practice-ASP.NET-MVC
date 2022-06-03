fa3c5a76d4e7a572d799aece6acfacf807ae2cd3
Pablo Ferreyra <ferreyp_adm@caribank.org>
2022-06-03 17:34:15 -0300

    first commit

---
+++ ManejoPresupuesto/ManejoPresupuesto/Controllers/TiposCuentasController.cs
@@ -0,0 +1,29 @@
+﻿using ManejoPresupuesto.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManejoPresupuesto.Controllers
+{
+    public class TiposCuentasController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Crear()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Crear(TipoCuenta tipoCuenta) 
+        {
+            if (!ModelState.IsValid) 
+            {//le reenvio el mismo para rellenarle el formulario con lo que envio
+
+                return View(tipoCuenta);
+            }
+            return View();
+        }
+    }
+}
---
+++ ManejoPresupuesto/ManejoPresupuesto/Models/TipoCuenta.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuesto.Models
+{
+    public class TipoCuenta
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="El campo {0} es requerido")]
+        [StringLength(maximumLength: 50, MinimumLength = 3, ErrorMessage ="La longitud del campo {0} debe estar entre {2} y {1}")]
+        [Display(Name = "nombre del tipo cuenta")]
+        public string Nombre { get; set; }
+        public int UsuarioId { get; set; }
+        public int Orden { get; set; }
+
+        /*Pruebas de otras validaciones por defecto*/
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [EmailAddress(ErrorMessage ="El campo debe ser un mail valido")]
+        public string Email { get; set; }
+        [Range(minimum:18, maximum: 130, ErrorMessage ="El valor debe estar entre {1} y {2}")]
+        public int Edad { get; set; }
+        [Url(ErrorMessage ="El campo debe ser una URL valida")]
+        public string URL { get; set; }
+        [CreditCard(ErrorMessage ="La tarjeta de credito no es valida")]
+        public string TarjetaDeCredito { get; set; }
+    }
+}
